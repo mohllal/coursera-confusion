22b43eefe53dd457a6e48f16d3d3bae07ae3fe16
KAREEM-PC\Kareem <kareem.mohllal@gmail.com>
2017-08-22 00:58:51 +0200

    Add register, login, and logout routes

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var index = require('./routes/index');
+var users = require('./routes/users');
+
+var User = require('./models/users');
+var config = require('./config');
 
 var app = express();
+
+mongoose.connect(config.mongoUrl);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	// we're connected!
+	console.log("Connected correctly to the mongoDB server");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,9 +36,17 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 app.use(cookieParser());
+
+// passport config
+app.use(passport.initialize());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/users.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+
+var User = require('../models/users');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	User.find({}, function(err, users) {
+		if (err) throw err;
+		res.json(users);
+	});
+});
+
+router.post('/register', function(req, res) {
+	User.register(new User({
+			username: req.body.username,
+			firstName: req.body.firstname,
+			lastName: req.body.lastname
+		}),
+		req.body.password,
+		function(err, user) {
+			if (err) {
+				return res.status(500).json({
+					err: err
+				});
+			}
+
+			if (req.body.age) {
+				user.age = req.body.age;
+			}
+			if (req.body.gender) {
+				user.gender = req.body.gender;
+			}
+
+			user.save(function(err, user) {
+				passport.authenticate('local')(req, res, function() {
+					return res.status(200).json({
+						status: 'Registration Successful!'
+					});
+				});
+			});
+		});
+});
+
+router.post('/login', function(req, res, next) {
+	passport.authenticate('local', function(err, user, info) {
+		if (err) {
+			return next(err);
+		}
+		if (!user) {
+			return res.status(401).json({
+				err: info
+			});
+		}
+		req.logIn(user, function(err) {
+			if (err) {
+				return res.status(500).json({
+					err: 'Could not log in user'
+				});
+			}
+
+			res.status(200).json({
+				status: 'Login successful!',
+				success: true
+			});
+		});
+	})(req, res, next);
+});
+
+router.get('/logout', function(req, res) {
+	req.logout();
+	res.status(200).json({
+		status: 'Logout successful!'
+	});
+});
+
+module.exports = router;
