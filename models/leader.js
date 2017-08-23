d4e425aa3143099ed3c2a2dee97c6538587a52b9
KAREEM-PC\Kareem <kareem.mohllal@gmail.com>
2017-08-24 00:26:17 +0200

    Add leaders model

---
+++ models/leader.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var leaderSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	image: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	designation: {
+		type: String,
+		required: true,
+	},
+	abbr: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true
+	}
+}, {
+	timestamps: true
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var Leaders = mongoose.model('Leader', leaderSchema);
+
+module.exports = Leaders;
