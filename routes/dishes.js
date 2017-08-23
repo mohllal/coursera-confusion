d0865e4e92e28216b1d9dc32b4f45be47ba78179
KAREEM-PC\Kareem <kareem.mohllal@gmail.com>
2017-08-23 06:10:22 +0200

    Add comments collection routes

--- routes/dishes.js
+++ routes/dishes.js
@@ -75,4 +75,45 @@ router.route('/:dishId')
 		});
 	});
 
+router.route('/:dishId/comments')
+	.get(verify.verifyOrdinaryUser, function(req, res, next) {
+		Dish.findById(req.params.dishId)
+			.populate('comments.postedBy')
+			.exec(function(err, dish) {
+				if (err) throw err;
+				res.json(dish.comments);
+			});
+	})
+
+	.post(verify.verifyOrdinaryUser, function(req, res, next) {
+		Dish.findById(req.params.dishId, function(err, dish) {
+			if (err) throw err;
+			req.body.postedBy = req.decoded._doc._id;
+			dish.comments.push(req.body);
+			dish.save(function(err, dish) {
+				if (err) throw err;
+				console.log('Updated comments of the dish with id: ' + dish._id);
+				res.json(dish);
+			});
+		});
+	})
+
+	.delete(verify.verifyOrdinaryUser, verify.verifyAdmin, function(req, res, next) {
+		Dish.findById(req.params.dishId, function(err, dish) {
+			if (err) throw err;
+			for (var i = (dish.comments.length - 1); i >= 0; i--) {
+				dish.comments.id(dish.comments[i]._id).remove();
+			}
+			dish.save(function(err, dish) {
+				if (err) throw err;
+
+				var id = dish._id;
+				res.writeHead(200, {
+					'Content-Type': 'text/plain'
+				});
+				res.end('Successfully deleted the dish with id: ' + id);
+			});
+		});
+	});
+
 module.exports = router;
