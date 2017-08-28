b376d2f09f35407537842478a3c8e334a0e44fc1
KAREEM-PC\Kareem <kareem.mohllal@gmail.com>
2017-08-29 01:47:57 +0200

    Add header AngularJS template

---
+++ public/javascripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('confusionApp', ['ui.router', 'ngResource', 'ngDialog'])
+	.config(function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			// route for the home page
+			.state('app', {
+				url: '/',
+				views: {
+					'header': {
+						templateUrl: 'templates/header.html'
+					}
+				}
+			});
+
+		$urlRouterProvider.otherwise('/');
+	});
