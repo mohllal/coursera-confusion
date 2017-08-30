f8224abcd2226b81743fd0acfc765ddbf4201429
KAREEM-PC\Kareem <kareem.mohllal@gmail.com>
2017-08-30 03:46:24 +0200

    Add login controller

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -11,4 +11,25 @@ angular.module('confusionApp')
 			AuthFactory.register($scope.registration);
 			ngDialog.close();
 		};
+	}])
+
+	.controller('LoginController', ['$scope', 'ngDialog', '$localStorage', 'AuthFactory', function($scope, ngDialog, $localStorage, AuthFactory) {
+		$scope.loginData = $localStorage.getObject('userinfo', '{}');
+
+		$scope.doLogin = function() {
+			if ($scope.rememberMe)
+				$localStorage.storeObject('userinfo', $scope.loginData);
+
+			AuthFactory.login($scope.loginData);
+			ngDialog.close();
+		};
+
+		$scope.openRegister = function() {
+			ngDialog.open({
+				template: 'templates/register.html',
+				scope: $scope,
+				className: 'ngdialog-theme-default',
+				controller: "RegisterController"
+			});
+		};
 	}]);
